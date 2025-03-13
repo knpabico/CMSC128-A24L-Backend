448e1a7376afc932dc77c099e797b515e7e7eeeb
Justin-Roy-Santos <justinroysantos@gmail.com>
2025-03-13 14:49:48 +0800

    feat: Create basic /admin-dasboard page and disable routes protection

---
+++ src/app/admin-dashboard/page.tsx
@@ -0,0 +1,40 @@
+import { Breadcrumbs } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
+
+export default async function AdminDashboard() {
+  const links = [
+    { label: " Manage Users", link: "manage-users" },
+    { label: "Organize Events", link: "organize-events" },
+    { label: "Send Newsletters", link: "send-newsletters" },
+    { label: "Monitor Engagement Metrics", link: "engagement-metrics" },
+    { label: "Site Settings", link: "site-settings" },
+  ];
+
+  return (
+    <>
+      <Breadcrumbs
+        items={[
+          {
+            label: "Dashboard",
+          },
+        ]}
+      />  
+
+      {/* page title */}
+      <h1 className="text-4xl font-bold my-6">Admin Dashboard</h1>
+
+      {/* temporary buttons */}
+      {/* create a button for each admin-control link */}
+      <div className="flex flex-col gap-4">
+
+      {links.map((item, i) => (
+        <Button asChild key={i} className="text-lg h-14">
+          <Link href={`/admin-dashboard/${item.link}`}>{item.label}</Link>
+        </Button>
+      ))}
+      </div>
+    </>
+  );
+}
