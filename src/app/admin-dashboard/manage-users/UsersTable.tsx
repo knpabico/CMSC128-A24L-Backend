d520174bb5066f74071898cc0cd5c9dae2ca3cd1
Justin-Roy-Santos <justinroysantos@gmail.com>
2025-03-13 23:07:32 +0800

    feat: Use the function for querying alumni data and enable the regStatus filter and pagination options

--- src/app/admin-dashboard/manage-users/UsersTable.tsx
+++ src/app/admin-dashboard/manage-users/UsersTable.tsx
@@ -1,19 +1,54 @@
 import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { getAlumni } from "@/data/alumni";
-import { DeleteIcon, EyeIcon, PencilIcon } from "lucide-react";
+import { RegStatus } from "@/types/alumni/regStatus";
+import {
+  DeleteIcon,
+  EyeIcon,
+  PencilIcon,
+  Settings,
+  Trash2Icon,
+} from "lucide-react";
 import Link from "next/link";
 
 // this table will only render the data in the specified page
-export async function UsersTable({ page = 1 }: { page?: number }) {
-  // query users (alumni) data
-  const {data, totalPages} = await getAlumni({
-    pagination: {
-      page,
-      pageSize: 3,
-    }
-  });
-
+export async function UsersTable({
+  page = 1,
+  regStatus = undefined,
+}: {
+  page?: number;
+  regStatus?: RegStatus | undefined;
+}) {
+  let response;
+  if (regStatus) {
+    // query users (alumni) data
+    response = await getAlumni({
+      filters: {
+        regStatus
+      },
+      pagination: {
+        page,
+        pageSize: 3,
+      },
+    });
+  } else {
+    response = await getAlumni({
+      pagination: {
+        page,
+        pageSize: 3,
+      },
+    });
+  }
+  const {data, totalPages} = response;
+    
   return (
     <>
       {!data ? (
@@ -32,25 +67,27 @@ export async function UsersTable({ page = 1 }: { page?: number }) {
               <TableHead>Student Number</TableHead>
               <TableHead>Registration Status</TableHead>
               <TableHead></TableHead>
-              <TableHead />
             </TableRow>
           </TableHeader>
           <TableBody>
             {/* create a row in the table for each user/alumni */}
-            {data.map(alumni => {
+            {data.map((alumni) => {
               return (
                 <TableRow key={alumni.id}>
                   <TableCell>{alumni.name}</TableCell>
                   <TableCell>{alumni.email}</TableCell>
                   <TableCell>{alumni.studentNumber}</TableCell>
                   <TableCell>{alumni.regStatus}</TableCell>
-                  <TableCell>
-                    <Button asChild variant='outline' size='sm'>
-                        <DeleteIcon />
-                    </Button> /
-                    <Button asChild variant='outline' size='sm'>
-                        <EyeIcon />
-                    </Button> / 
+                  <TableCell className="flex gap-3">
+                    <Button variant="outline" size="sm">
+                      <Trash2Icon />
+                    </Button>
+                    <Button variant="outline" size="sm">
+                      <Settings />
+                    </Button>
+                    <Button variant="outline" size="sm">
+                      <EyeIcon />
+                    </Button>
                   </TableCell>
                 </TableRow>
               );
@@ -63,7 +100,10 @@ export async function UsersTable({ page = 1 }: { page?: number }) {
               <TableCell colSpan={5} className="text-center">
                 {Array.from({ length: totalPages }).map((_, i) => (
                   <Button key={i} asChild variant="outline" className="mx-1">
-                    <Link href={`/admin-dashboard/manage-users?page=${i + 1}`}>{i + 1}</Link>
+                    {/* put a search param in the url indicating the page, and navigate to it */}
+                    <Link href={`/admin-dashboard/manage-users?page=${i + 1}${regStatus ? `&status=${regStatus}` : ''}`}>
+                      {i + 1}
+                    </Link>
                   </Button>
                 ))}
               </TableCell>
@@ -72,6 +112,5 @@ export async function UsersTable({ page = 1 }: { page?: number }) {
         </Table>
       )}
     </>
-    
   );
 }
--- src/app/admin-dashboard/manage-users/page.tsx
+++ src/app/admin-dashboard/manage-users/page.tsx
@@ -1,9 +1,24 @@
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
 import { UsersTable } from "./UsersTable";
+import { Button } from "@/components/ui/button";
+import { redirect } from 'next/navigation';
+import { RegStatus } from "@/types/alumni/regStatus";
+import Link from "next/link";
 
-const title = 'Manage Users';
+const title = "Manage Users";
 
-export default function ManageUsers() {
+// retrieve the search params which will be used for the pagination of the table
+export default async function ManageUsers({
+  searchParams,
+}: {
+  searchParams: Promise<any>;
+}) {
+  const searchParamsValue = await searchParams;
+  // get the value of the page property in the query param/string
+  const page = searchParamsValue?.page ? parseInt(searchParamsValue.page) : 1;
+  const status: RegStatus | undefined = searchParamsValue?.status
+    ? searchParamsValue.status
+    : undefined;
 
   return (
     <>
@@ -19,11 +34,17 @@ export default function ManageUsers() {
         ]}
       />
 
-      {/* page title */}
       <h1 className="text-4xl font-bold my-6">{title}</h1>
 
       {/* users table */}
-      <UsersTable />
+      <UsersTable page={page} regStatus={status} />
+
+      <div className="flex justify-center gap-5 mt-5">
+      <a className="text-blue-500 underline" href='/admin-dashboard/manage-users?page=1'>All</a>
+      <a className="text-blue-500 underline" href='/admin-dashboard/manage-users?page=1&status=pending'>pending</a>
+      <a className="text-blue-500 underline" href='/admin-dashboard/manage-users?page=1&status=rejected'>rejected</a>
+      <a className="text-blue-500 underline" href='/admin-dashboard/manage-users?page=1&status=approved'>approved</a>
+      </div>
     </>
   );
 }
