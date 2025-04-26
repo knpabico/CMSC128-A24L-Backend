441c5f683512ae2a2cce8b8b042129cec3d3a11c
Denisse Alcantara <dbalcantara@up.edu.ph>
2025-04-26 23:33:07 +0800

    style(ui): re-organized newsletter details according to category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,7 +159,9 @@ export default function Home() {
                       key={index}
                       className="flex flex-col rounded-[10px] mb-[10px] w-150 h-auto p-[20px] bg-[#FFFFFF] border border-[#DADADA]"
                     >
-                      <div className="flex flex-row mb-[10px]  gap-2 items-center">
+
+                      {/* user info */}
+                      <div className="flex flex-row mb-[20px]  gap-2 items-center">
                         <img
                           src="https://i.pinimg.com/736x/14/e3/d5/14e3d56a83bb18a397a73c9b6e63741a.jpg"
                           className="w-10 h-10 object-cover object-top rounded-full border border-[#DADADA]"
@@ -197,7 +199,6 @@ export default function Home() {
                         return jobOffering ? (
                           
                           <div className="flex flex-col gap-[30px]">
-                            {/* <h1 className="text-[24px] font-bold">{jobOffering.title}</h1> */}
                             <div className="flex flex-col gap-[1px]">
                               <p className="text-[24px] font-semibold">
                                 {jobOffering.position} 
@@ -206,50 +207,52 @@ export default function Home() {
                                 {jobOffering.company} 
                               </p>
                             </div>
-                            <div className="flex flex-col gap-[5px]">
-                              <div className="flex flex-row gap-[5px]">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[20px] w-auto">
-                                  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-                                </svg>
-                                <p className="text-[15px]">
-                                  {jobOffering.employmentType}
-                                </p>
-                                <p>&#xb7;</p>
-                                <p className="text-[15px]">
-                                {jobOffering.experienceLevel}</p>
-                              </div>
-
-                              <div className="flex flex-row gap-[5px]">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[20px] w-auto">
-                                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
-                                </svg>
-                                <p className="text-[15px]">
-                                  {jobOffering.salaryRange}
-                                </p>
-                              </div>
-                              
-                              <div className="text-[15px]"> 
-                                <div className="flex flex-row gap-[3px]">
+                            <div className="flex flex-col gap-[30px]">
+                              <div className="flex flex-col gap-[10px]">
+                                <div className="flex flex-row gap-[5px]">
                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[20px] w-auto">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
                                   </svg>
-                                    Required skill(s):
+                                  <p className="text-[15px]">
+                                    {jobOffering.employmentType}
+                                  </p>
+                                  <p>&#xb7;</p>
+                                  <p className="text-[15px]">
+                                  {jobOffering.experienceLevel}</p>
                                 </div>
 
-                                <div className="pl-[25px]">
-                                  {jobOffering.requiredSkill.map((skill: string) => (
-                                  <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-500"> 
-                                    <li key={skill} className="flex items-center" >
-                                    <svg className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                <div className="flex flex-row gap-[5px]">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[20px] w-auto">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                                  </svg>
+                                  <p className="text-[15px]">
+                                    {jobOffering.salaryRange}
+                                  </p>
+                                </div>
+                              
+                                <div className="text-[15px]"> 
+                                  <div className="flex flex-row gap-[3px]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[20px] w-auto">
+                                      <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
                                     </svg>
-                                    {skill}
-                                    </li>
-                                  </ul>))}
-                                </div>
-                              </div>  
-                            </div>
+                                      Required skill(s):
+                                  </div>
 
+                                  <div className="pl-[25px]">
+                                    {jobOffering.requiredSkill.map((skill: string) => (
+                                    <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-500"> 
+                                      <li key={skill} className="flex items-center" >
+                                      <svg className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                                      </svg>
+                                      {skill}
+                                      </li>
+                                    </ul>))}
+                                  </div>
+                                </div>
+                              </div> 
+                            
+                            {/* job desc */}
                             <div>
                               <div className="flex flex-row items-center gap-[5px]">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-[20px] w-auto">
@@ -258,10 +261,11 @@ export default function Home() {
                                 <p className="text-[20px] font-semibold">About the job</p>
                               </div>
                             
-                                <p className="text-[15px] ml-[25px] text-justify">
-                                  {jobOffering.jobDescription}
-                                </p>
-                              </div> 
+                              <p className="text-[15px] ml-[25px] text-justify">
+                                {jobOffering.jobDescription}
+                              </p>
+                            </div> 
+
                             <div className="flex flex-row w-full gap-[10px]">
                               <button
                                 onClick={() => router.push(`/joboffer-list`)}
@@ -272,7 +276,10 @@ export default function Home() {
                                 onClick={() => router.push(`/joboffer-list`)}
                                 className="w-full h-[30px] rounded-full border border-[1px] border-[#0856BA] bg-[#0856BA] text-[#FFFFFF] text-[12px] hover:bg-[#FFFFFF] hover:text-[#0856BA]"
                               >Apply</button>
+                            </div>          
                             </div>
+
+                            
                             
                           </div>
                         ) : (
