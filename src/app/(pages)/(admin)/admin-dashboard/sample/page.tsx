ee93221730004c5d53beb306a82f9b3d735298c1
zdmaiso <zomaiso@up.edu.ph>
2025-04-29 17:42:50 +0800

    style: switch button

--- src/app/(pages)/(admin)/admin-dashboard/sample/page.tsx
+++ src/app/(pages)/(admin)/admin-dashboard/sample/page.tsx
@@ -26,6 +26,11 @@ export default function SampleAdminPage() {
     { title: "Donation Drive #15", details: "Donation Drive Details" },
   ];
 
+	const [toggles, setToggles] = useState(
+		donationDrives.map(() => false) // initialize all to false
+	);
+	
+
   return (
     <div className="flex flex-col gap-5">
       <div className="w-full">
@@ -57,7 +62,7 @@ export default function SampleAdminPage() {
               <div
                 className={`w-full py-3 flex items-center justify-center rounded-t-2xl font-semibold text-base ${
                   activeTab === tab
-                    ? "text-blue-500 bg-white"
+                    ? "text-[var(--primary-blue)] bg-white"
                     : "text-blue-200 bg-white"
                 }`}
               >
@@ -109,14 +114,27 @@ export default function SampleAdminPage() {
                   <div className="text-sm text-gray-600">{drive.details}</div>
                 </div>
                 <div className="w-2/3 flex items-center justify-end p-5">
+								<div className="w-1/6 flex items-center justify-center">
+  							<div
+											onClick={() =>
+												setToggles((prev) =>
+													prev.map((val, i) => (i === index ? !val : val))
+												)
+											}
+											className={`w-12 h-6 flex items-center rounded-full p-1 cursor-pointer transition-colors ${
+												toggles[index] ? "bg-[var(--primary-blue)]" : "bg-gray-300"
+											}`}
+										>
+											<div
+												className={`bg-white w-4 h-4 rounded-full shadow-md transform transition-transform ${
+													toggles[index] ? "translate-x-6" : "translate-x-0"
+												}`}
+											/>
+										</div>
+									</div>
+
                   <div className="w-1/6 flex items-center justify-center">
-                    <div className="bg-[var(--primary-blue)] rounded-full p-1 flex w-fit">
-                      <div className="bg-white rounded-full w-5 h-5"></div>
-                      <div className="bg-[var(--primary-blue)] rounded-full w-5 h-5"></div>
-                    </div>
-                  </div>
-                  <div className="w-1/6 flex items-center justify-center">
-                    <div className="text-blue-500 hover:underline cursor-pointer">View Details</div>
+                    <div className="text-[var(--primary-blue)] hover:underline cursor-pointer">View Details</div>
                   </div>
                   <div className="w-1/6 flex items-center justify-center">
                     <Trash2 size={20} className="text-gray-500 hover:text-red-500 cursor-pointer" />
