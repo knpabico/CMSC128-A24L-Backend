cf8d9b61c797c89d098645c80705221010e2b19c
alarconMT <mtalarcon1@up.edu.ph>
2025-04-29 20:43:00 +0800

    refactor: removed pending and rejected status in details page of admin side

--- src/app/(pages)/(admin)/admin-dashboard/donation-drive/details/page.tsx
+++ src/app/(pages)/(admin)/admin-dashboard/donation-drive/details/page.tsx
@@ -275,13 +275,17 @@ const getRemainingDays = (endDate: any) => {
             {/* Title */}
             <div className="flex justify-between items-start">
                 <h1 className="text-5xl font-bold text-gray-800">{donationDrive.isEvent && event ? event.title : donationDrive.campaignName}</h1>
-                <span className={`px-3 py-1 text-sm rounded-full ${
+                {/* <span className={`px-3 py-1 text-sm rounded-full ${
                     donationDrive.status === 'active' ? 'bg-green-100 text-green-800' :
                     donationDrive.status === 'pending' ? 'bg-yellow-100 text-yellow-800' :
                     donationDrive.status === 'completed' ? 'bg-blue-100 text-blue-800' :
                     donationDrive.status === 'rejected' ? 'bg-red-100 text-red-800' :
-                    'bg-gray-100 text-gray-800'
-                    
+                    'bg-gray-100 text-gray-800'  
+                }`}> */}
+                <span className={`px-3 py-1 text-sm rounded-full ${
+                    donationDrive.status === 'active' ? 'bg-green-100 text-green-800' :
+                    donationDrive.status === 'completed' ? 'bg-blue-100 text-blue-800' :
+                    'bg-gray-100 text-gray-800'  
                 }`}>
                     {donationDrive.status.charAt(0).toUpperCase() + donationDrive.status.slice(1)}
                 </span>
