0bdc06e0c39cc072d79d8ff0bd2fa7e5b7ea1035
ALRnoche <arnoche1@up.edu.ph>
2025-04-29 16:33:41 +0800

    chore: formatted code with better readability

--- src/app/(pages)/(alumni)/events/[eventId]/page.tsx
+++ src/app/(pages)/(alumni)/events/[eventId]/page.tsx
@@ -6,7 +6,8 @@ import { useRsvpDetails } from "@/context/RSVPContext";
 import { Event, RSVP } from "@/models/models";
 import { useAuth } from "@/context/AuthContext";
 
-const EventPageAlumni = () => {
+const EventPageAlumni = () => 
+{
   const { events } = useEvents();
   const { rsvpDetails, isLoadingRsvp, handleAlumAccept, handleAlumReject } = useRsvpDetails(events);
   const { alumInfo } = useAuth();
@@ -16,7 +17,8 @@ const EventPageAlumni = () => {
   const eventId = params?.eventId as string;
   const event = events.find((e: Event) => e.eventId === eventId);
 
-  if (!eventId || events.length === 0) {
+  if (!eventId || events.length === 0)
+  {
     return <p>Loading...</p>;
   }
 
@@ -68,10 +70,15 @@ const EventPageAlumni = () => {
                   Going
                 </button>
                 <button
-                  onClick={() => {
-                    if (alumInfo?.alumniId) {
+                  onClick={() =>
+                  {
+                    if (alumInfo?.alumniId)
+                    {
                       handleAlumReject(event.eventId, alumInfo.alumniId);
-                    } else {
+                    } 
+                    
+                    else
+                    {
                       console.log("Alumni ID is not available.");
                     }
                   }}
@@ -83,7 +90,8 @@ const EventPageAlumni = () => {
             ) : null} {/* This ensures that when the RSVP is not "Pending", nothing is displayed */}
           </div>
           <button
-            onClick={() => {
+            onClick={() =>
+            {
               router.push(`/donationdrive-list/details?id=${event.donationDriveId}`);
             }}
             className="px-4 py-2 bg-gray-500 text-white rounded-md"
