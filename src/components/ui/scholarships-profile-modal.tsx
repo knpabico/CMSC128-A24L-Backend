0bac6ebad27dd9fc9ae3b71c816e3314e36f44f2
adpaleracio <adpaleracio@up.edu.ph>
2025-05-15 00:43:29 +0800

    dummy ng record of scholars

---
+++ src/components/ui/scholarships-profile-modal.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+
+
+export default function ScholarshipsProfile()
+  {
+    return(
+    <div className="mx-50 mt-10 mb-15">
+        hello
+    </div>
+    )
+};
