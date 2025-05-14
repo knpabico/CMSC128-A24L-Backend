418a82026291f6586033dd25a6fd3074b3d6f466
elcartedave <deybelcarte@gmail.com>
2025-05-15 00:36:40 +0800

    chore: added a suspense for not found page

---
+++ src/components/NotFoundContent.tsx
@@ -0,0 +1,25 @@
+"use client";
+import LoadingPage from "@/components/Loading";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function NotFoundContent() {
+  const { isGoogleSignIn, status, isAdmin, loading } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (isGoogleSignIn) {
+      router.push("/sign-up");
+    } else if (status !== "approved" && !isAdmin && !loading) {
+      router.push("/login");
+    }
+  }, [status, isAdmin, router, loading, isGoogleSignIn]);
+  if (status === "approved" || isAdmin) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen text-2xl font-bold">
+        Page Not Found
+      </div>
+    );
+  }
+  return <LoadingPage />;
+}
