b4b83844a89cd0c41c6ddda0e4117984136e9e39
evangelistapau <pmevangelista1@up.edu.ph>
2025-05-15 01:09:41 +0800

    fix: shows current selected on view details page

--- src/app/(pages)/(admin)/admin-dashboard/organize-events/[eventId]/page.tsx
+++ src/app/(pages)/(admin)/admin-dashboard/organize-events/[eventId]/page.tsx
@@ -178,19 +178,11 @@ export default function EventPageAdmin() {
       // Properly check targetGuests for alumni and batches
       if (eventToEdit.targetGuests && eventToEdit.targetGuests.length > 0) {
         // Check if the first item is a batch (e.g., a string of length 4)
-        if (eventToEdit.inviteType === "batch") {
-          const selectedInfo = Array.from(new Set(
-            alums
-              .filter((alumni: { alumniId: any }) => eventToEdit.targetGuests.includes(alumni.alumniId))
-              .map((alumni: { studentNumber: string | any[] }) => alumni.studentNumber?.slice(0, 4))
-          )) as string[];// Set the batches
-          setSelectedBatches(selectedInfo)
+        if (eventToEdit.targetGuests[0].length === 4) {
+          setSelectedBatches(eventToEdit.targetGuests) // Set the batches
           setVisibility("batch") // Set visibility to batches
-        } else if (eventToEdit.inviteType === "alumni") {
-          const selectedInfo = alums
-            .filter((alumni: { alumniId: any }) => eventToEdit.targetGuests.includes(alumni.alumniId))
-            .map((alumni: { email: any }) => alumni.email);// Set the batches
-          setSelectedAlumni(selectedInfo) // Set the alumni
+        } else {
+          setSelectedAlumni(eventToEdit.targetGuests) // Set the alumni
           setVisibility("alumni") // Set visibility to alumni
         }
       }
