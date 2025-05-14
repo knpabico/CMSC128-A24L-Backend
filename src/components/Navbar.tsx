38bb67e3e655c2dc17751cffb2186459ca9eb991
vgtolentino <vgtolentino1@up.edu.ph>
2025-05-14 20:54:40 +0800

    fix: add job posting admin route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -128,7 +128,7 @@ export default function Navbar() {
           label: "Manage Job Posting",
           path: "/admin-dashboard/job-postings",
         },
-        { id: "add-jobs", label: "Add Job Posting", path: "/admin/jobs/add" },
+        { id: "add-jobs", label: "Add Job Posting", path: "/admin-dashboard/job-postings/post" },
         {
           id: "stats-jobs",
           label: "Statistical Report",
